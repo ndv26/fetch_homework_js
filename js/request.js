3857b9da0ad636682667874c2fd5371e043c7c05
Nguyen Duc Vuong <vuong260902@gmail.com>
2022-05-27 22:18:36 +0700

    Refactor request functions into async await contruct

--- js/request.js
+++ js/request.js
@@ -1,54 +1,82 @@
 const apiUrl = "https://api-d.thesoftwarehouse.tech/api/i-users/";
 
-export function getUsersHandler(render) {
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((responseData) => {
-            render(responseData.data);
-        });
+export async function getUsersHandler(render) {
+    try {
+        const response = await fetch(apiUrl);
+        const responseData = await response.json();
+        if (!response.ok) {
+            throw new Error(responseData.error.message || "Something went wrong!");
+        }
+        render(responseData?.data);
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
-export function addUserHandler(userData, render) {
+export async function addUserHandler(formInputs, addNewRow) {
     const httpConfig = {
         method: "POST",
         body: JSON.stringify({
-            data: { ...userData },
+            data: { ...formInputs },
         }),
         headers: { "Content-Type": "application/json" },
     };
-    fetch(apiUrl, httpConfig)
-        .then((response) => response.json())
-        .then((user) => {
-            render(user.data);
-        });
+
+    try {
+        const response = await fetch(apiUrl, httpConfig);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            throw new Error(responseData.error.message || "Something went wrong!");
+        }
+
+        addNewRow(responseData?.data);
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
-export function updateUserHandler(id, userData, selectedRow) {
+export async function updateUserHandler(id, formInputs, selectedRow) {
     const httpConfig = {
         method: "PUT",
         body: JSON.stringify({
             data: {
-                ...userData,
+                ...formInputs,
             },
         }),
         headers: { "Content-Type": "application/json" },
     };
-    fetch(apiUrl + id, httpConfig)
-        .then((response) => response.json())
-        .then((responseData) => {
-            selectedRow.cells[0].innerHTML = responseData.data.attributes.username;
-            selectedRow.cells[1].innerHTML = responseData.data.attributes.email;
-            selectedRow.cells[2].innerHTML = responseData.data.attributes.firstName;
-            selectedRow.cells[3].innerHTML = responseData.data.attributes.lastName;
-        });
+
+    try {
+        const response = await fetch(apiUrl + id, httpConfig);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            throw new Error(responseData.error.message || "Something went wrong!");
+        }
+        selectedRow.cells[0].innerHTML = responseData?.data?.attributes?.username;
+        selectedRow.cells[1].innerHTML = responseData?.data?.attributes?.email;
+        selectedRow.cells[2].innerHTML = responseData?.data?.attributes?.firstName;
+        selectedRow.cells[3].innerHTML = responseData?.data?.attributes?.lastName;
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
-export function deleteUserHandler(id) {
+export async function deleteUserHandler(id) {
     const httpConfig = {
         method: "DELETE",
     };
-    fetch(apiUrl + id, httpConfig).then(() => {
+
+    try {
+        const response = await fetch(apiUrl + id, httpConfig);
+        const responseData = await response.json();
+        if (!response.ok) {
+            throw new Error(responseData.error.message || "Something went wrong!");
+        }
         const delRow = document.querySelector(".user-" + id);
         delRow.remove();
-    });
+    } catch (error) {
+        alert(error.message);
+    }
 }
